411ab5f9ef29196c8d897c61c05cff2164975f84
grillika <grillika@mail.ru>
2016-09-19 23:24:48 +0300

    validation is almost done

--- main.js
+++ main.js
@@ -12,18 +12,90 @@ var paymentForm = (function () {
             return num == 5 || num == 6;
         });
 
-        var subjects = {
+        var _subjects = {
             "Tatarstan": ["Kazan", "Naberezhnye Chelny"],
             "Chuvashia": ["Cheboksary", "Kanash"]
         }
-        model.availableRegions = Object.keys(subjects);
+        model.availableRegions = Object.keys(_subjects);
         model.region = ko.observable("");
         model.availableCities = ko.computed(function () {
-            return subjects[model.region()];
+            return _subjects[model.region()];
         });
         model.city = ko.observable("");
 
+       
+        var requiredFields = [];
 
+        model.submit = function (formElement) {
+        
+
+          
+        }
+
+    }
+
+
+    function AlertModel(head, message) {
+        this.head = head;
+        this.message = message;
+    }
+
+    function AlertsManager(templateId, defaultTargetId) {
+        this._template = $('#' + templateId);
+        this._defaultTargetId = defaultTargetId;
+    }
+    AlertsManager.prototype.showNewDanger = function (error, targetId) {
+        targetId = targetId || this._defaultTargetId;
+        this._template.tmpl(error).insertAfter('#' + targetId);
+    }
+    AlertsManager.prototype.removeAll = function () {
+        $(".alert").alert("close");
+    }
+
+    function Validator(alertsManager) {
+        this.alertsManager = alertsManager;
+    }
+    Validator.prototype._requiredMessage = 'The field i required';
+    Validator.prototype._mustBeNumericMessage = 'The field must be numeric';
+    Validator.prototype._emailNotValidMessage = 'Email is not valid';
+    Validator.prototype._isEmpty = function (val) {
+        return val.lenght === 0;
+    }
+    Validator.prototype._isNumbers = function (val) {
+        return !isNaN(val);
+    }
+    Validator.prototype._isEmail = function (val) {
+        return /^[-\w.]+@([A-z0-9][-A-z0-9]+\.)+[A-z]{2,4}$/.test(val);
+    }
+    Validator.prototype.checkRequired = function (requiredFields, model) {
+        var result = true;
+        for (var field in requiredFields) {
+            if (this._isEmpty(model[field]())) {
+                result = false;
+                this.alertsManager.showNewDanger(new AlertModel('', this._requiredMessage), field);
+            }
+        }
+        return result;
+    }
+    Validator.prototype.checkNumeric = function (numericFields, model) {
+        var result = true;
+        for (var field in numericFields) {
+            if (!this._isNumbers(model[field]())) {
+                result = false;
+                this.alertsManager.showNewDanger(new AlertModel('', this._mustBeNumericMessage), field);
+            }
+        }
+        return result;
+    }
+    Validator.prototype.checkEmails = function (emailFields, model) {
+        var result = true;
+        for (var field in emailFields) {
+            if (!this._isEmail(model[field]())) {
+                result = false;
+                this.alertsManager.showNewDanger(new AlertModel('', this._emailNotValidMessage), field);
+            }
+        }
+        return result;
     }
 
 
