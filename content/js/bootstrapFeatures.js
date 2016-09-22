7e52513e65d1cc4cc66d4560de205c559fbaed32
grillika <grillika@mail.ru>
2016-09-22 17:01:00 +0300

    modules

---
+++ content/js/bootstrapFeatures.js
@@ -0,0 +1,44 @@
+var bootstrapFeatures = (function () {
+    function AlertModel(head, message) {
+        this.head = head;
+        this.message = message;
+    }
+
+    function AlertsManager(templateId, defaultTargetId) {
+        this._template = $(templateId);
+        this._defaultTargetId = defaultTargetId;
+    }
+    AlertsManager.prototype.showNewDanger = function (message, head, targetId) {
+        targetId = targetId || this._defaultTargetId;
+        head = head || '';
+        this._template.tmpl(new AlertModel(head, message)).insertAfter(targetId);
+    }
+    AlertsManager.prototype.removeAll = function () {
+        $(".alert").alert("close");
+    }
+
+
+    function inputStatesManager(validTemplateId, invalidTemplateId) {
+        this.validView = $(validTemplateId).tmpl(),
+        this.invalidView = $(invalidTemplateId).tmpl();
+    }
+    inputStatesManager.prototype.modes = {
+        valid: { view: this.validView, parentClass: 'has-success' },
+        invalid: { view: this.invalidView, parentClass: 'has-error' }
+    }
+    inputStatesManager.prototype._parentClassesToRemove =
+        $.map(inputStatesManager.prototype.modes, function (item) {
+            return item.parentClass;
+        }).join(' ');
+    inputStatesManager.prototype.mark = function (inputId, mode) {
+        var input = $(inputId);
+        input.insertAfter(mode.view);
+        input.parent().removeClass(this._parentClassesToRemove).addClass(mode.parentClass);
+    }
+
+
+    return {
+        AlertsManager: AlertsManager,
+        inputStatesManager: inputStatesManager
+    }
+})();
--- content/js/main.js
+++ content/js/main.js
@@ -6,7 +6,7 @@ var paymentForm = (function () {
         var validator = new formValidation.Validator();            
 
         var requiredFields = ['region', 'city', 'cardNumber', 'cardMonth', 'cardYear', 'CSC', 'firstName', 'lastName', 'addressLine1', 'phone', 'email'],
-            numericFields   = ['cardNumber', 'cardMonth', 'cardYear', 'CSC', 'phone'],
+            numericFields = ['cardNumber', 'cardMonth', 'cardYear', 'CSC', 'phone'],
             emailFields = ['email'],
             allValidatedFields = requiredFields.concat(numericFields.concat(emailFields));
 
@@ -112,89 +112,4 @@ var paymentForm = (function () {
 
     });
 
-})();
-
-
-
-
-var bootstrapFeatures = (function () {
-    function AlertModel(head, message) {
-        this.head = head;
-        this.message = message;
-    }
-
-    function AlertsManager(templateId, defaultTargetId) {
-        this._template = $(templateId);
-        this._defaultTargetId = defaultTargetId;
-    }
-    AlertsManager.prototype.showNewDanger = function (message, head, targetId) {
-        targetId = targetId || this._defaultTargetId;
-        head = head || '';
-        this._template.tmpl(new AlertModel(head, message)).insertAfter(targetId);
-    }
-    AlertsManager.prototype.removeAll = function () {
-        $(".alert").alert("close");
-    }
-
-
-    function inputStatesManager(validTemplateId, invalidTemplateId) {
-        this.validView = $(validTemplateId).tmpl(),
-        this.invalidView = $(invalidTemplateId).tmpl();
-    }
-    inputStatesManager.prototype.modes = {
-        valid: { view: this.validView, parentClass: 'has-success' },
-        invalid: { view: this.invalidView, parentClass: 'has-error' }
-    }
-    inputStatesManager.prototype._parentClassesToRemove =
-        $.map(inputStatesManager.prototype.modes, function (item) {
-            return item.parentClass;
-        }).join(' ');
-    inputStatesManager.prototype.mark = function (inputId, mode) {
-        var input = $(inputId);
-        input.insertAfter(mode.view);
-        input.parent().removeClass(this._parentClassesToRemove).addClass(mode.parentClass);
-    }
-
-
-    return {
-        AlertsManager: AlertsManager,
-        inputStatesManager: inputStatesManager
-    }
-})();
-
-
-
-
-var formValidation = (function () {
-
-    function Validator() { }
-
-    Validator.prototype._isNotEmpty = function (val) {
-        return val.length !== 0;
-    }
-    Validator.prototype._isNumbers = function (val) {
-        return !isNaN(val);
-    }
-    Validator.prototype._isEmail = function (val) {
-        return /^[-\w.]+@([A-z0-9][-A-z0-9]+\.)+[A-z]{2,4}$/.test(val);
-    }
-    Validator.prototype.modes = {
-        required: { check: Validator.prototype._isNotEmpty },
-        numeric: { check: Validator.prototype._isNumbers },
-        email: { check: Validator.prototype._isEmail }
-    }
-
-    Validator.prototype.getInvalidFields = function (fieldsToValidate, model, validationMode) {
-        var result = [];
-        fieldsToValidate.forEach(function (item, i, arr) {
-            if (!validationMode.check(model[item]())) {
-                result.push(item);
-            }
-        });
-        return result;
-    }
-
-    return {
-        Validator: Validator
-    }
 })();
