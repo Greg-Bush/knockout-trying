b9360b6ce4bf9dc54d5598297583f9aa114ab191
grillika <grillika@mail.ru>
2016-09-26 13:48:22 +0300

    cleaning

--- js/application/formValidation.js
+++ js/application/formValidation.js
@@ -18,19 +18,7 @@ define(
         Validator.prototype.isEmail = function (val) {
             return Validator.prototype.isNotEmpty(val) && /^[-\w.]+@([A-z0-9][-A-z0-9]+\.)+[A-z]{2,4}$/.test(val);
         }
-
-        Validator.prototype.getInvalidFields = function (validateModelArray, modelWithObservableFields) {
-            var result = [];
-            validateModelArray.forEach(function (model) {
-                model.fields.forEach(function (field) {
-                    if (!model.check(modelWithObservableFields[field]())) {
-                        result.push(field);
-                    }
-                });
-            });
-            return result;
-        }
-
+        
         return {
             Validator: Validator,
             ValidateModel: ValidateModel
